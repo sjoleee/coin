ab6216299d00903acacbddf3c949570316babc43
이상조 <isangjo@isangjoui-MacBookPro.local>
2022-07-05 17:34:51 +0900

    coin list app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [coinlist, setCoinlist] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((res) => res.json())
+      .then((json) => {
+        setCoinlist(json);
+        setLoading(false);
+      });
+  }, []);
+  console.log(coinlist);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>coins</h1>
+      {loading ? <strong>Loading....</strong> : null}
+      <div>
+        {coinlist.map((coin) => (
+          <p>
+            {coin.name}({coin.symbol}) : {coin.quotes.USD.price}
+          </p>
+        ))}
+      </div>
     </div>
   );
 }
